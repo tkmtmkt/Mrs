45050b9e48ef2d03744a8dd0dd5890ebe2ced4d8
yosuk <yosuk@DESKTOP-72IVPTC>
2018-11-25 01:54:46 +0900

    git init

---
+++ Mrs/src/main/java/mrs/domain/repository/room/MeetingRoomRepository.java
@@ -0,0 +1,9 @@
+package mrs.domain.repository.room;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import mrs.domain.model.MeetingRoom;
+
+public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Integer> {
+
+	MeetingRoom findOne(Integer roomId);
+}
