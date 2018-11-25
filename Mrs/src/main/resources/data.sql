ba357f2cb6b9e33321efa22813fa0568cf3a1a76
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-11-25 10:33:45 +0900

    fix: Linux上で実行するための修正

--- Mrs/src/main/resources/data.sql
+++ Mrs/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO usr (user_id, first_name, last_name, role_name, password)
+INSERT INTO usr (user_id, first_name, last_name, role_name, password)
 VALUES ('taro-yamada', '太郎', '山田', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
 INSERT INTO usr (user_id, first_name, last_name, role_name, password)
 VALUES ('aaaa', 'Aaa', 'Aaa', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
